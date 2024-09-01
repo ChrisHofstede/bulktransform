f4c54196dd54f48ae85734a4b8b68437850f29a4
Chris Hofstede <chris-hofstede@outlook.com>
2024-09-01 18:02:45 +0200

    Add command line options

--- src/main/java/nl/chrishofstede/bulktransform/App.java
+++ src/main/java/nl/chrishofstede/bulktransform/App.java
@@ -1,13 +1,28 @@
 package nl.chrishofstede.bulktransform;
 
+import org.apache.commons.cli.*;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    final static Options options = new Options();
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        options.addOption(new Option("in", "input", true, "input file name (wildcards allowed)"));
+        options.addOption(new Option("xsl", "xslt", true, "XSLT stylesheet input file name"));
+        options.addOption(new Option("out", "output", true, "output folder"));
+
+        // create the parser
+        CommandLineParser parser = new DefaultParser();
+        try {
+            // parse the command line arguments
+            CommandLine line = parser.parse(options, args);
+        } catch (ParseException exp) {
+            // oops, something went wrong
+            System.err.println("Parsing failed.  Reason: " + exp.getMessage());
+        }
     }
 }
--- src/main/java/nl/chrishofstede/bulktransform/Stylesheet.java
+++ src/main/java/nl/chrishofstede/bulktransform/Stylesheet.java
@@ -2,7 +2,6 @@ package nl.chrishofstede.bulktransform;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
@@ -25,7 +24,6 @@ import nl.chrishofstede.bulktransform.utils.Parameters;
 import nl.chrishofstede.bulktransform.utils.TransformerErrorListener;
 import nl.chrishofstede.bulktransform.utils.TransformerLoggingErrorListener;
 
-
 public class Stylesheet implements Serializable {
 
 	private Templates translet = null;
@@ -33,197 +31,174 @@ public class Stylesheet implements Serializable {
 	/**
 	 * Gets a transformer instance of an specific XSLT stylesheet.
 	 * 
-	 * @param stylesheet
-	 *            Stylesheet used for the transformation.
-	 * @return An instance of the Transformer class can transform a source tree into a result tree.
-	 * @throws Exception
-	 *             Signals that a non user recoverable error has occurred.
+	 * @return An instance of the Transformer class can transform a source tree into
+	 *         a result tree.
+	 ** @throws Exception
+	 *                   Signals that a non user recoverable error has occurred.
 	 */
-	public Transformer getTransformer(String stylesheet) throws Exception {
-		if (stylesheet == null) {
-			throw new IllegalArgumentException("stylesheet is null");
+	private Transformer getTransformer() throws Exception {
+		if (translet == null) {
+			throw new Exception("No translet for returning a transformer");
 		}
 
-		// Retrieve the stylesheet and return a transformer
-		final Templates templates = getTranslet(stylesheet);
-		return templates.newTransformer();
+		// Retrieve the translet and return a transformer
+		return translet.newTransformer();
 	}
 
 	/**
-	 * Gets an XSL stylesheet translet from a file.
-	 *  
+	 * Sets an XSL stylesheet translet from a file.
+	 * 
 	 * @param stylesheet
-	 *            Stylesheet file name.
+	 *                   Stylesheet file name.
 	 * @return Compiled stylesheet Templates.
 	 * @throws Exception
-	 *             Signals that a non user recoverable error has occurred.
+	 *                   Signals that a non user recoverable error has occurred.
 	 */
-	public Templates getTranslet(String stylesheet) throws Exception {
+	public void setTranslet(String stylesheet) throws Exception {
 		if (stylesheet == null) {
 			throw new IllegalArgumentException("stylesheet is null");
 		}
 
-		// Get compiled stylesheet 
-		if (translet == null) {
-			// Compiled stylesheet not available, retrieve it from the local
-			// filesystem (stylesheets folder) or from the database
+		// Create a compiled stylesheet via a transformer factory
+		// Make sure Xalan is used
+		final TransformerFactory tFactory = TransformerFactory.newInstance(
+				"org.apache.xalan.processor.TransformerFactoryImpl", null);
 
-			// Create a compiled stylesheet via a transformer factory
-			// Make sure Xalan is used
-			final TransformerFactory tFactory = TransformerFactory.newInstance(
-					"org.apache.xalan.processor.TransformerFactoryImpl", null);
-
-			final File lf = new File(stylesheet);
-			if (lf.exists()) {
-				// Load the stylesheet from local file
-				try (InputStream fis = new FileInputStream(lf)) {
-					final Source xslt = new StreamSource(fis);
-					xslt.setSystemId(stylesheet);
-					translet = tFactory.newTemplates(xslt);
-					if (translet == null)
-							throw new Exception("Could not parse style sheet (see previous error messages for details)");
-				}
-			
+		final File lf = new File(stylesheet);
+		if (lf.exists()) {
+			// Load the stylesheet from local file
+			try (InputStream fis = new FileInputStream(lf)) {
+				final Source xslt = new StreamSource(fis);
+				xslt.setSystemId(stylesheet);
+				translet = tFactory.newTemplates(xslt);
+				if (translet == null)
+					throw new Exception("Could not parse style sheet (see previous error messages for details)");
 			}
 		}
-
-		// Throw IdsServletException if stylesheet has not been found which
-		// eases error handling.
-		if (translet == null) {
-			throw new FileNotFoundException("Stylesheet not found or loaded: " + stylesheet);
-		}
-
-		// Return a compiled stylesheet
-		return translet;
 	}
 
 	/**
-	 * Transforms the content of an XML source to a node by using an XSLT stylesheet.
+	 * Transforms the content of an XML source to a node by using an XSLT
+	 * stylesheet.
 	 * 
 	 * @param node
-	 *            Node to be transformed.
-	 * @param stylesheet
-	 *            Stylesheet used for the transformation.
+	 *                   Node to be transformed.
 	 * @param parameters
-	 *            Properties object with transformation parameters.
+	 *                   Properties object with transformation parameters.
 	 * @param result
-	 *            Transformation result output node.
+	 *                   Transformation result output node.
 	 * @param log
-	 *            Writer to receive the transformer log output.
+	 *                   Writer to receive the transformer log output.
 	 * @throws Exception
-	 *             Signals that a non user recoverable error has occurred.
+	 *                   Signals that a non user recoverable error has occurred.
 	 */
-	public void transformNodeToNode(Node node, String stylesheet, Parameters parameters, Node result, Writer log)
+	public void transformNodeToNode(Node node, Parameters parameters, Node result, Writer log)
 			throws Exception {
 		if (node == null || result == null) {
 			throw new IllegalArgumentException("node or result is null");
 		}
-		transformSourceToResult(new DOMSource(node), stylesheet, parameters, new DOMResult(result), log);
+		transformSourceToResult(new DOMSource(node), parameters, new DOMResult(result), log);
 	}
 
 	/**
-	 * Transforms the content of an XML source to a node by using an XSLT stylesheet.
+	 * Transforms the content of an XML source to a node by using an XSLT
+	 * stylesheet.
 	 * 
 	 * @param node
-	 *            Node to be transformed.
-	 * @param stylesheet
-	 *            Stylesheet used for the transformation.
+	 *                   Node to be transformed.
 	 * @param parameters
-	 *            Properties object with transformation parameters.
+	 *                   Properties object with transformation parameters.
 	 * @param result
-	 *            Transformation result output node.
+	 *                   Transformation result output node.
 	 * @param log
-	 *            Writer to receive the transformer log output.
+	 *                   Writer to receive the transformer log output.
 	 * @throws Exception
-	 *             Signals that a non user recoverable error has occurred.
+	 *                   Signals that a non user recoverable error has occurred.
 	 */
-	public void transformNodeToResult(Node node, String stylesheet, Parameters parameters, Result result,
+	public void transformNodeToResult(Node node, Parameters parameters, Result result,
 			Writer log) throws Exception {
 		if (node == null || result == null) {
 			throw new IllegalArgumentException("node or result is null");
 		}
-		transformSourceToResult(new DOMSource(node), stylesheet, parameters, result, log);
+		transformSourceToResult(new DOMSource(node), parameters, result, log);
 	}
 
 	/**
-	 * Transforms the content of an XML source to a stream by using an XSLT stylesheet.
+	 * Transforms the content of an XML source to a stream by using an XSLT
+	 * stylesheet.
 	 * 
 	 * @param node
-	 *            Node to be transformed.
-	 * @param stylesheet
-	 *            Stylesheet used for the transformation.
+	 *                   Node to be transformed.
 	 * @param parameters
-	 *            Properties object with transformation parameters.
+	 *                   Properties object with transformation parameters.
 	 * @param result
-	 *            Transformation result output stream.
+	 *                   Transformation result output stream.
 	 * @param log
-	 *            Writer to receive the transformer log output.
+	 *                   Writer to receive the transformer log output.
 	 * @throws Exception
-	 *             Signals that a non user recoverable error has occurred.
+	 *                   Signals that a non user recoverable error has occurred.
 	 */
-	public void transformNodeToStream(Node node, String stylesheet, Parameters parameters, OutputStream result,
+	public void transformNodeToStream(Node node, Parameters parameters, OutputStream result,
 			Writer log) throws Exception {
 		if (node == null || result == null) {
 			throw new IllegalArgumentException("node or result is null");
 		}
-		transformSourceToResult(new DOMSource(node), stylesheet, parameters, new StreamResult(result), log);
+		transformSourceToResult(new DOMSource(node), parameters, new StreamResult(result), log);
 	}
 
 	/**
-	 * Transforms the content of an XML source to a writer by using an XSLT stylesheet.
+	 * Transforms the content of an XML source to a writer by using an XSLT
+	 * stylesheet.
 	 * 
 	 * @param node
-	 *            Node to be transformed.
-	 * @param stylesheet
-	 *            Stylesheet used for the transformation.
+	 *                   Node to be transformed.
 	 * @param parameters
-	 *            Properties object with transformation parameters.
+	 *                   Properties object with transformation parameters.
 	 * @param result
-	 *            Transformation result output stream.
+	 *                   Transformation result output stream.
 	 * @param log
-	 *            Writer to receive the transformer log output.
+	 *                   Writer to receive the transformer log output.
 	 * @throws Exception
-	 *             Signals that a non user recoverable error has occurred.
+	 *                   Signals that a non user recoverable error has occurred.
 	 */
-	public void transformNodeToWriter(Node node, String stylesheet, Parameters parameters, StringWriter result,
+	public void transformNodeToWriter(Node node, Parameters parameters, StringWriter result,
 			Writer log) throws Exception {
 		if (node == null || result == null) {
 			throw new IllegalArgumentException("node or result is null");
 		}
-		transformSourceToResult(new DOMSource(node), stylesheet, parameters, new StreamResult(result), log);
+		transformSourceToResult(new DOMSource(node), parameters, new StreamResult(result), log);
 	}
 
 	/**
-	 * Transforms the content of an XML document in a DOM by using an XSLT stylesheet.
+	 * Transforms the content of an XML document in a DOM by using an XSLT
+	 * stylesheet.
 	 * 
 	 * @param node
-	 *            Node to be transformed.
-	 * @param stylesheet
-	 *            Stylesheet used for the transformation.
+	 *                   Node to be transformed.
 	 * @param parameters
-	 *            Properties object with transformation parameters.
+	 *                   Properties object with transformation parameters.
 	 * @return The transformed document as a string.
 	 * @throws Exception
-	 *             Signals that a non user recoverable error has occurred.
+	 *                   Signals that a non user recoverable error has occurred.
 	 */
-	public String transformNodeToString(Node node, String stylesheet, Parameters parameters) throws Exception {
+	public String transformNodeToString(Node node, Parameters parameters) throws Exception {
 		if (node == null) {
 			throw new IllegalArgumentException("node is null");
 		}
 		final StringWriter stringWriter = new StringWriter();
 
 		// Transform XML and place the result in a string
-		transformNodeToWriter(node, stylesheet, parameters, stringWriter, null);
+		transformNodeToWriter(node, parameters, stringWriter, null);
 
 		// Return the result as string
 		return stringWriter.toString();
 	}
 
-	private void transformSourceToResult(Source source, String stylesheet, Parameters parameters, Result result,
+	private void transformSourceToResult(Source source, Parameters parameters, Result result,
 			Writer log) throws Exception {
 
 		// Obtain a transformer
-		final Transformer transformer = getTransformer(stylesheet);
+		final Transformer transformer = getTransformer();
 
 		// Set an logging error listener if logging is required
 		if (log != null) {
